3e4a6563b31d9edef991ac8a564ef0f816184724
Shaikhmohddanish <danishshaikh2365@gmail.com>
2026-01-05 12:43:40 +0530

    update Google Analytics gtag ID to AW-17847118434

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <head>
         {/* Google tag (gtag.js) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17632885577"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17847118434"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -38,7 +38,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17632885577');
+            gtag('config', 'AW-17847118434');
           `}
         </Script>
       </head>
